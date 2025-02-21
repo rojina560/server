0b0df1392043970cbfd362f9363762e214029ab3
Your Name <you@example.com>
2025-02-21 11:18:44 +0600

    project setup

--- index.js
+++ index.js
@@ -8,14 +8,8 @@ const port = process.env.PORT || 5000;
 // middleware 
 app.use(cors())
 app.use(express.json())
-// 
-// 
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cdbyl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,7 +20,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
   
     // Send a ping to confirm a successful connection
